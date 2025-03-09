8f2cac62b1392e03370d7e9b8aaf499fbf8ccfcd
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-03-09 20:19:07 -0300

    Criacao MS para gerenciamento do DB com flyway

---
+++ dev/back/DBManager/src/test/java/br/com/mobiauto/DBManager/DbManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mobiauto.DBManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
